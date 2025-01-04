30248d2ae6a892410b2dd40c7ca04d159cc00674
Conor Brady <conorpbrady@gmail.com>
2025-01-04 09:51:47 -0500

    add basic daily calendar view to right pane

---
+++ entente/react_frontend/src/Components/DailyView.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './DailyView.css'
+
+
+export default function DailyView() {
+  return (
+    <div className={"daily-view"}>
+    {[...Array(24).keys()].map((hour) => <HourEntry hour={hour} />)}
+    </div>
+  );
+};
+
+function HourEntry({ hour }: { hour: number }): React.JSX.Element {
+  return (
+    <div className={"hour-card"}>
+    <div className={"hour-head"}>
+    <span>{`${hour}:00`}</span>
+    </div>
+    <div className={"hour-body"}>
+    </div>
+    <div className={"hour-body"}>
+    </div>
+    </div>
+  );
+};
--- entente/react_frontend/src/Components/RightPane.tsx
+++ entente/react_frontend/src/Components/RightPane.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './LeftPane.css';
+import DailyView from './DailyView';
 
 export default function RightPane() {
   return (
     <div className={"right-pane"}>
-      <p>Right Content</p>
+    <DailyView />
       </div>
   );
 };
